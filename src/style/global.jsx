58deb8792ecf577fec1bbc0a9a1c40e9900d0f8f
n0eyes <doll4586633@naver.com>
2023-01-03 22:01:31 +0900

    style: 글로벌 스타일 설정

---
+++ src/style/global.jsx
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  html {
+    font-size: 62.5%;
+  }
+  
+  *{
+    box-sizing: border-box;
+  }
+
+  input:-webkit-autofill {
+   -webkit-box-shadow: 0 0 0 1000px #ffffff inset;
+  }
+`;
+
+export { GlobalStyle };
