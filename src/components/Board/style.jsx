372c12aedea74ea330fbee7efc7df746f4707388
n0eyes <doll4586633@naver.com>
2023-01-05 07:29:11 +0900

    style: Board layout 설정

--- src/components/Board/style.jsx
+++ src/components/Board/style.jsx
@@ -1,7 +1,22 @@
 import styled from 'styled-components';
 
 const Styled = {
-  Root: styled.div``,
+  Root: styled.div`
+    width: 100rem;
+    min-height: 50rem;
+    max-height: 60rem;
+
+    padding: 3rem;
+    border: 1px solid lightgray;
+
+    position: absolute;
+    left: 50%;
+    top: 50%;
+
+    transform: translate(-50%, -50%);
+
+    overflow-y: scroll;
+  `,
 
   Title: styled.header`
     min-width: 10rem;
